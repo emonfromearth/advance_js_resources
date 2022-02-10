2415940cfaac3fb3a85a30862f3b2611cfdebae2
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-02-10 11:23:18 +0600

    caching using memoization

--- advance_js_sec7_FP.js
+++ advance_js_sec7_FP.js
@@ -160,9 +160,56 @@ To minimize the complexity of our code we should do declarative coding.
 // const multiplyBy5 = curriedMultiply(5)
 // multiplyBy5(20)
 
-// partial application function takes one argument at the first call and the rest of the arguments at the second call. 
+// partial application function takes one argument at the first call and the rest of the arguments at the second call.
 // not like currying function that takes one argument at a time.
-//Partial Application
-const multiply = (a, b, c) => a * b * c
-const partialMultiplyBy5 = multiply.bind(null, 5)
-partialMultiplyBy5(10, 20)
+// //Partial Application
+// const multiply = (a, b, c) => a * b * c
+// const partialMultiplyBy5 = multiply.bind(null, 5)
+// partialMultiplyBy5(10, 20)
+
+//learn to cache using memoization
+function addTo80(n) {
+  return n + 80;
+}
+
+addTo80(5); // this is not memoized
+
+// let's make the above function memoized
+let cache = {};
+function memoizeAddTo80(n) {
+  if (n in cache) {
+    return cache[n];
+  } else {
+    console.log("long time");
+    const answer = n + 80;
+    cache[n] = answer;
+    return answer;
+  }
+}
+
+// console.log(1, memoizeAddTo80(6))
+// // console.log(cache)
+// // console.log('-----------')
+// console.log(2, memoizeAddTo80(6))
+
+// let's make that better with no global scope. This is closure in javascript so.
+function memoizeAddTo80(n) {
+  let cache = {}; // here cache is a closure. it won't run every time and won't be reset.
+  return function (n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      console.log("long time");
+      const answer = n + 80;
+      cache[n] = answer;
+      return answer;
+    }
+  };
+}
+
+const memoized = memoizeAddTo80();
+console.log(1, memoized(6));
+// console.log(cache)
+// console.log('-----------')
+console.log(2, memoized(6)); // this will not run again because it's in the cache.
+console.log(3, memoized(8)); // this will run again because it's not in the cache.
